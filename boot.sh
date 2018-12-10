78b8e55d9c99ae2d2bb5bbb785347e94823e58bc
chenze <chenze@levelup.ai>
2018-12-10 11:04:11 +0800

    os & log

--- boot.sh
+++ boot.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 file=${1##*/}
 export SUB_START=${file%.*}
-export OS_NAME="android"
+system_name=$(uname -s)
+if [ $system_name == "Darwin" ]; then
+	export OS_NAME="mac"
+elif [ $system_name == "Linux" ]; then
+	export OS_NAME="linux"
+fi
 ./skynet/skynet config
 exit
